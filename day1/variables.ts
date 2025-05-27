8bf7c7896665bf8ba1bf37257b89221a2440172e
Muhammad Sadiq Anwer Khan <Muhammad Sadiq Anwer Khan>
2025-05-27 16:33:24 +0200

    day1: covered topics

--- day1/variables.ts
+++ day1/variables.ts
@@ -1,1 +1,24 @@
-console.log("Hello Typescript!");
+// Type annotations
+let age: number = 25;
+let firstName: string = "Sadiq";
+let isStudent: boolean = true;
+
+//Type inference (auto - detect types)
+let city = "Berlin";
+let score = 90;
+
+// SPECIAL TYPES - any, unknown, null, undefined
+let something: any = "can be anything";
+something = 42; // allowed
+
+//this is safer than any
+let notSure: unknown = "could be anything";
+
+let nothing: null = null;
+let notAssigned: undefined = undefined;
+
+// Arrays and Tuples
+let numbers: number[] = [1, 2, 3];
+let names: string[] = ["Ali", "Sara", "Zara"];
+
+let tuple: [string, number] = ["Age", 30];
